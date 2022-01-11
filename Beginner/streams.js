f55e61cd8bfb254f24e6a4cd96d89d866f94a6f9
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2022-01-11 23:39:46 +0500

    👌 IMPROVE: minor refactoring

--- Beginner/streams.js
+++ Beginner/streams.js
@@ -5,12 +5,12 @@ const readStream = fs.createReadStream("./docs/readStream.txt", { encoding: "utf
 // Write stream:
 const writeStream = fs.createWriteStream("./docs/writeStream.txt");
 
-// readStream.on("data", (chunk) => {
-//     console.log("-------New Chunk of Data---------");
-//     console.log(chunk);
-//     writeStream.write("\nNew Chunk\n");
-//     writeStream.write(chunk);
-// });
+readStream.on("data", (chunk) => {
+    console.log("-------New Chunk of Data---------");
+    console.log(chunk);
+    writeStream.write("\nNew Chunk\n");
+    writeStream.write(chunk);
+});
 
 // piping:
 readStream.pipe(writeStream);
