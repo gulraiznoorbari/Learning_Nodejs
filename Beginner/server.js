abe2241e85df1632fb8b43b345f98a887f80bb15
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2022-01-12 00:12:39 +0500

    📦 NEW: Redirecting

--- Beginner/server.js
+++ Beginner/server.js
@@ -16,10 +16,17 @@ const server = http.createServer((req, res) => {
             path += "about.html";
             res.statusCode = 200;
             break;
+
+        // redirecting:
+        case "/about-us":
+            res.statusCode = 301;
+            res.setHeader("Location", "/about");
+            res.end();
+            break;
+
         default:
             path += "404.html";
             res.statusCode = 404;
-            break;
     }
 
     // sending a html file:
