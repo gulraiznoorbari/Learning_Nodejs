2c1fd18a16156eedc68b3b799c495a33ac9846d0
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2022-01-13 22:26:38 +0500

    📦 NEW: redirect in express app

--- Beginner/express_app.js
+++ Beginner/express_app.js
@@ -13,3 +13,13 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.sendFile("./views/about.html", { root: __dirname });
 });
+
+// redirect:
+app.get("/about-us", (req, res) => {
+    res.redirect("/about");
+});
+
+// 404 page / middleware function:
+app.use((req, res) => {
+    res.sendFile("./views/404.html", { root: __dirname });
+});
